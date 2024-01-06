e5a0aee1dc1703317cfee7bf77fd18aa816fd4ab
aziya22 <aziyasolkar786@gmail.com>
2024-01-06 17:11:21 +0530

    Modified config.py

---
+++ configuration/config.py
@@ -0,0 +1,9 @@
+class TestData:
+    BROWSER = 'chrome'
+    CHROME_EXCEUTABLE_PATH = ""
+
+    URL = "https://www.saucedemo.com/"
+    USERNAME = "error_user"
+    PASSWORD = "secret_sauce"
+
+    IMPLICIT_WAIT = 10
